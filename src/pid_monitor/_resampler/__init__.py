4296cfdd4c24fe5601592542265d1e4c3d737c22
yuzj <theafamily@126.com>
2022-06-16 17:19:58 +0800

    Very basic support of aggregation on memory implemented

--- src/pid_monitor/_resampler/__init__.py
+++ src/pid_monitor/_resampler/__init__.py
@@ -2,6 +2,7 @@ import glob
 import math
 import os
 import re
+import shutil
 from typing import Tuple, Optional
 
 import pandas as pd
@@ -22,6 +23,44 @@ def get_first_and_last_timestamp_from_a_file(path: str) -> Optional[Tuple[float,
         return retd_start, retd_end
     else:
         return None
+
+
+def resample(output_basename: str):
+    """TODO: To be changed"""
+    rsc = ResamplerConfig.from_dir(output_basename, interval=1, round_to_demical=0)
+    files_needed_to_be_parsed = glob.glob(os.path.join(output_basename, "*.mem.tsv"))
+    for path in files_needed_to_be_parsed:
+        if path.find("sys") != -1:
+            continue
+        try:
+            BaseResampler(rsc).resample(pd.read_table(path)).to_csv(
+                path.replace(".tsv", ".resampled.tsv"),
+                sep="\t",
+                index_label="TIME"
+            )
+        except:
+            pass
+    files_needed_to_be_parsed = glob.glob(os.path.join(output_basename, "*.mem.resampled.tsv"))
+    full_df = None
+    for path in files_needed_to_be_parsed:
+        print(path)
+        df = pd.read_table(path).set_index("TIME").fillna(value=0)
+        if full_df is None:
+            full_df = df
+        else:
+            full_df = full_df.join(df, on="TIME", lsuffix="_L", rsuffix="_R")
+            for name in (
+                "VIRT",
+                "RESIDENT",
+                "SHARED",
+                "TEXT",
+                "DATA",
+                "SWAP"
+            ):
+                full_df[name] = full_df[f"{name}_L"] + full_df[f"{name}_R"]
+                full_df = full_df.drop([f"{name}_R", f"{name}_L"], axis=1)
+                #print(full_df.dtypes.keys())
+    print(full_df)
 
 
 class ResamplerConfig:
@@ -52,6 +91,8 @@ class ResamplerConfig:
         new_instance_end = -math.inf
         files_needed_to_be_parsed = glob.glob(os.path.join(output_basename, "*.tsv"))
         for path in files_needed_to_be_parsed:
+            if path.find("resampled") != -1:
+                continue
             this_time_start_end = get_first_and_last_timestamp_from_a_file(path)
             if this_time_start_end is None:
                 continue
@@ -83,7 +124,6 @@ class BaseResampler:
         The Default resampler will resample all data
         """
         df['TIME'] = pd.to_datetime(df['TIME'], unit="s")
-        print(df.head())
         df_time_start = df['TIME'][0]
         df_time_end = df['TIME'].iloc[-1]
         df_scale_start = self.rsc.index.array[self.rsc.index.array.searchsorted(df_time_start, side="right")]
@@ -93,16 +133,12 @@ class BaseResampler:
             end=df_scale_end,
             freq=self.rsc.interval
         )
-        df = df.set_index('TIME')
-
-        df = df.reindex(phase1_scale_index, method="bfill").reindex(self.rsc.index)
+        df = df.set_index('TIME').reindex(phase1_scale_index, method="bfill").reindex(self.rsc.index)
         return df
 
 
 if __name__ == '__main__':
-    rc = ResamplerConfig.from_dir(
-        "/home/yuzj/Documents/gpmf/opt/proc_profiler/src/pid_monitor/_test/proc_profiler_test",
-        1.0,
-        2
+    resample(
+        "/home/yuzj/Documents/gpmf/opt/proc_profiler/src/pid_monitor/_test/proc_profiler_test"
     )
-    print(rc.__dict__)
+
